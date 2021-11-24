30902227c0a6351cc74504f436ec384155a45d56
selfsinger <meghasv1234@gmail.com>
2021-11-24 19:49:48 +0530

    Invoking assignment

---
+++ car/src/com/xworkz/anotherclss/Otherclass.java
@@ -0,0 +1,16 @@
+package com.xworkz.anotherclss;
+
+import com.xworkz.car.Car;
+
+public class Otherclass {
+	public static void main(String args[])
+	{
+		Car car=new Car();
+		System.out.println(Car.brand);
+		System.out.println(Car.carname);
+		car.method1();
+		
+		
+		
+	}
+}
---
+++ car/src/com/xworkz/car/Car.java
@@ -0,0 +1,22 @@
+package com.xworkz.car;
+
+public class Car {
+ 
+	public static String brand="BMW";
+	public static String carname="BMWM4";
+	int price=5000000;
+	
+	public void method1()
+	{
+		System.out.println("first method");		
+	}
+	 void method2()
+	 {
+		 System.out.println("second method");
+	 }
+	 private void method3()
+	 {
+		 System.out.println("third method");
+	
+	 }
+}
